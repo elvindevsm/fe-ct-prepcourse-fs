0425ca4ca43796dcb9819e7d54291887ef4200bb
elvinSm <elvindevelopersm@gmail.com>
2023-06-09 10:36:48 -0500

    Ultima modificacion en modulo 7 y desarrollo del M8(FIN)

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,13 +6,32 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor(nombre,apellido,edad,domicilio){
+      this.nombre = nombre;
+      this.apellido= apellido;
+      this.edad = edad
+      this.domicilio = domicilio;
+   }
+   detalle(){
+     return {nombre:this.nombre,apellido:this.apellido,edad:this.edad,domicilio:this.domicilio};
+
+   }
 }
+
+const yoMismo =new Persona('elvin','Simanca',22,'Cr 7 a sur #46');
+
+yoMismo.detalle();
+
+
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   const instanciaPersona = new Persona('Yulianis','cañas',22,'cr 25 sur #45');
+   return instanciaPersona;
+
 }
 
 function agregarMetodo() {
@@ -20,6 +39,13 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   class saludo extends Persona{
+      constructor(nombre,edad){
+         super(nombre,edad);
+      }
+      datos(){return `${this.nombre}+${this.edad} años.` }
+   }
+   return saludo;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
